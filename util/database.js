dac61cdcfb0918fa6361884a6fc4373b3fee76ba
arunkamboj <akamboj4@gmail.com>
2026-05-28 00:56:56 +0530

    Added port

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_SCHEMA_NAME,process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
   dialect: "mysql",
   host: process.env.DB_HOST_URL,
+  port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
